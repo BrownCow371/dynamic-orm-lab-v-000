0225025fd6072e18b62eb9b978e6757f688f912c
Jamie Brown <jmbrowncow@gmail.com>
2018-07-27 17:37:52 +0000

    automatically backed up by learn

--- config/console.rb
+++ config/console.rb
@@ -1,6 +1,6 @@
-require_relative "../lib/interactive_record"
-require_relative "../lib/student"
-require_relative "../config/environment"
+require_relative "../lib/interactive_record.rb"
+require_relative "../lib/student.rb"
+require_relative "../config/environment.rb"
 
 require "irb"
 IRB.start
