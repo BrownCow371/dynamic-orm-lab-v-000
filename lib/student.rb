6721daf073f71a66a5194fdcc94a07261376f0fc
Jamie Brown <jmbrowncow@gmail.com>
2018-07-27 17:40:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,7 @@
 require_relative "../config/environment.rb"
+require_relative "../lib/interactive_record.rb"
 require 'active_support/inflector'
-require 'interactive_record.rb'
+#require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
